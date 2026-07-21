249f9b0c2709b9988fc263ae4c2d3586258dcf97
Abd Latif  SIDI MED NABGHA <abdelatif.nabgha06@gmail.com>
2026-07-21 12:46:54 +0100

    Time: 0 ms (100.00%) | Memory: 43.5 MB (97.91%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,38 +1,22 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        
-        int m = matrix[0].length; // number of column
-        int n = matrix.length; // number of row
+        int m = matrix.length;      // rows
+        int n = matrix[0].length;   // cols
 
-        Integer targetRow = null;
-
-        int left = 0, right = n-1;
+        int left = 0, right = m * n - 1;
 
         while (left <= right) {
-            int mid = (left + right) / 2;
+            int mid = left + (right - left) / 2;
+            int row = mid / n;
+            int col = mid % n;
+            int val = matrix[row][col];
 
-            // check if target in that range row
-            if (matrix[mid][0] <= target && target <= matrix[mid][m-1]) {
-                targetRow = mid;
-                break;
-            }
-            else if (matrix[mid][0] > target) right = mid-1;
-            else left = mid+1;
-        }
-        if (targetRow == null) return false;
-
-        left = 0;
-        right = m-1;
-
-        while (left <= right)  {
-            int mid = (left + right) / 2;
-            if (matrix[targetRow][mid] == target) {
+            if (val == target) {
                 return true;
-            }
-            else if (matrix[targetRow][mid] > target) {
-                right = mid-1;
+            } else if (val < target) {
+                left = mid + 1;
             } else {
-                left = mid+1;
+                right = mid - 1;
             }
         }
         return false;
