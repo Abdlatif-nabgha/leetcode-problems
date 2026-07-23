4718d1f3bc386296f86038c8154224b9e505df31
Abd Latif  SIDI MED NABGHA <abdelatif.nabgha06@gmail.com>
2026-07-23 12:27:29 +0100

    Time: 6 ms (99.70%) | Memory: 48.2 MB (22.25%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,28 +1,29 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         int left = 1;
-        int right=piles[0];
-
-        for (int i = 1; i < piles.length; i++) {
-            right = Math.max(right, piles[i]);
+        int right = 0;
+        for (int pile : piles) {
+            right = Math.max(right, pile);
         }
 
-        int k = right;
-
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            int hours = 0;
-            for (int i = 0; i < piles.length; i++) {
-                hours +=  Math.ceil((double)piles[i] / mid);
-            }
-            if (hours <= h) {
-                k = Math.min(k, mid);
-                right = mid - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (canFinish(piles, h, mid)) {
+                right = mid;
             } else {
                 left = mid + 1;
             }
         }
 
-        return k;
+        return left;
+    }
+
+    private boolean canFinish(int[] piles, int h, int k) {
+        int hours = 0;
+        for (int pile : piles) {
+            hours += (pile + k - 1) / k;
+            if (hours > h) return false; // early exit
+        }
+        return hours <= h;
     }
 }
