c67b395faa202faa96a942096b1921c9bf243fac
Abd Latif  SIDI MED NABGHA <abdelatif.nabgha06@gmail.com>
2026-08-11 13:34:18 +0100

    Time: 0 ms (100.00%) | Memory: 43.3 MB (32.66%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,14 +1,26 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        String output = "";
         
-        for (int i = 0; i < strs[0].length(); i++) {
-            for (String s : strs) {
-                if (i == s.length() || s.charAt(i) != strs[0].charAt(i)) 
-                    return output;
+
+        //  Handle empty input array safely
+        if (strs == null || strs.length == 0) return "";
+        
+        
+        StringBuilder prefix = new StringBuilder(strs[0]);
+
+        int length = strs.length;
+
+
+       for (int i = 1; i < length; i++) {
+            
+            while (!strs[i].startsWith(prefix.toString())) {
+
+                if (prefix.length() == 0) return "";
+
+                prefix.deleteCharAt(prefix.length() - 1);
             }
-            output += strs[0].charAt(i);
-        }
-        return output;
+       }
+
+        return prefix.toString();
     }
 }
